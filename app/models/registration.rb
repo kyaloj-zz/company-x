2bbc94c9a089befad52d011e7ed094f2ea59dc58
Justus <mj.kyalo@gmail.com>
2018-09-08 11:02:27 +0300

    add validations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,6 +1,7 @@
 class Registration
   include ActiveModel::Model
   attr_accessor :company_name, :user_name, :user_email, :user_password
+  validates :company_name, :user_name, :user_email, :user_password, presence: true
 
   def register_user
     company = Company.create(name: company_name)
